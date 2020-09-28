ed10a8dae5f565a5afb41a6eebab5c89e00d52d8
Wh_Frost <whfrost63@gmail.com>
2020-09-28 14:26:00 +0400

    Разбил функцию рендера карточки на отдельные функции

--- js/main.js
+++ js/main.js
@@ -101,47 +101,66 @@ let renderPins = function () {
 };
 renderPins();
 
-let renderCard = function (obj) {
+let renderCardTemplate = function () {
   let card = CARD_TEMPLATE.cloneNode(true);
   MAP.insertBefore(card, FILTERS_CONTAINER);
-  let popupAvatar = card.querySelector(`.popup__avatar`);
-  let popupTitle = card.querySelector(`.popup__title`);
-  let popupAddress = card.querySelector(`.popup__text--address`);
-  let popupType = card.querySelector(`.popup__type`);
-  let popupPrice = card.querySelector(`.popup__text--price`);
+};
+let renderCardAvatar = function (obj) {
+  let popupAvatar = document.querySelector(`.popup__avatar`);
+  if (obj.author.avatar) {
+    popupAvatar.src = obj.author.avatar;
+  } else {
+    popupAvatar.remove();
+  }
+};
+let renderCardTitle = function (obj) {
+  let popupTitle = document.querySelector(`.popup__title`);
+  popupTitle.textContent = obj.offer.title;
+};
+let renderCardAddress = function (obj) {
+  let popupAddress = document.querySelector(`.popup__text--address`);
+  if (obj.offer.address) {
+    popupAddress.textContent = obj.offer.address;
+  } else {
+    popupAddress.remove();
+  }
+};
+let renderCardPrice = function (obj) {
+  let popupPrice = document.querySelector(`.popup__text--price`);
+  popupPrice.textContent = obj.offer.price + `₽/ночь`;
+};
+let renderCardType = function (obj) {
+  let popupType = document.querySelector(`.popup__type`);
   let typeTranslation = {
     palace: `Дворец`,
     flat: `Квартира`,
     house: `Дом`,
     bungalow: `Бунгало`
   };
-  let popupCapacity = card.querySelector(`.popup__text--capacity`);
-  let popupTime = card.querySelector(`.popup__text--time`);
-  let popupFeatures = card.querySelector(`.popup__features`);
-  let popupDescription = card.querySelector(`.popup__description`);
-  let popupPhoto = card.querySelector(`.popup__photos`);
-  if (obj.author.avatar) {
-    popupAvatar.src = obj.author.avatar;
-  } else {
-    popupAvatar.remove();
-  }
-  popupTitle.textContent = obj.offer.title;
-  if (obj.offer.address) {
-    popupAddress.textContent = obj.offer.address;
-  } else {
-    popupAddress.remove();
-  }
-  popupPrice.textContent = obj.offer.price + `₽/ночь`;
   if (obj.offer.type) {
     popupType.textContent = typeTranslation[obj.offer.type];
   } else {
     popupType.remove();
   }
+};
+let renderCardCapacity = function (obj) {
+  let popupCapacity = document.querySelector(`.popup__text--capacity`);
   if (obj.offer.rooms && obj.offer.guests) {
     popupCapacity.textContent = obj.offer.rooms + ` комнаты для ` + obj.offer.guests + ` гостей`;
   } else {
     popupCapacity.remove();
   }
+};
+let renderCardTime = function (obj) {
+  let popupTime = document.querySelector(`.popup__text--time`);
+  if (obj.offer.checkin && obj.offer.checkout) {
+    popupTime.textContent = `Заезд после ` + obj.offer.checkin + `, выезд до ` + obj.offer.checkout;
+  } else {
+    popupTime.remove();
+  }
+};
+let renderCardFeatures = function (obj) {
+  let popupFeatures = document.querySelector(`.popup__features`);
   while (popupFeatures.firstChild) {
     popupFeatures.firstChild.remove();
   }
@@ -158,16 +177,17 @@ let renderCard = function (obj) {
   } else {
     popupFeatures.remove();
   }
-  if (obj.offer.checkin && obj.offer.checkout) {
-    popupTime.textContent = `Заезд после ` + obj.offer.checkin + `, выезд до ` + obj.offer.checkout;
-  } else {
-    popupTime.remove();
-  }
+};
+let renderCardDescription = function (obj) {
+  let popupDescription = document.querySelector(`.popup__description`);
   if (obj.offer.description) {
     popupDescription.textContent = obj.offer.description;
   } else {
     popupDescription.remove();
   }
+};
+let renderCardPhoto = function (obj) {
+  let popupPhoto = document.querySelector(`.popup__photos`);
   while (popupPhoto.firstChild) {
     popupPhoto.firstChild.remove();
   }
@@ -188,4 +208,19 @@ let renderCard = function (obj) {
     popupPhoto.remove();
   }
 };
+
+let renderCard = function (obj) {
+  renderCardTemplate();
+  renderCardAvatar(obj);
+  renderCardTitle(obj);
+  renderCardAddress(obj);
+  renderCardPrice(obj);
+  renderCardType(obj);
+  renderCardCapacity(obj);
+  renderCardTime(obj);
+  renderCardFeatures(obj);
+  renderCardDescription(obj);
+  renderCardPhoto(obj);
+};
+
 renderCard(offersList[0]);
