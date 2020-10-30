16803fb1d0343d1dbb1e48e09ec17c5fb275a932
Wh_Frost <whfrost63@gmail.com>
2020-10-30 10:50:04 +0400

    правка поля адреса

--- js/move.js
+++ js/move.js
@@ -39,7 +39,7 @@
         }
         window.map.mainPin.style.top = newCoords.y + `px`;
         window.map.mainPin.style.left = newCoords.x + `px`;
-        window.form.address.value = newCoords.x + `, ` + (newCoords.y + window.map.mainPinOffset);
+        window.form.address.value = Math.floor(newCoords.x + (window.map.mainPinWidth / 2)) + `, ` + (newCoords.y + window.map.mainPinOffset);
       };
       let onMouseUp = function (upEvt) {
         upEvt.preventDefault();
